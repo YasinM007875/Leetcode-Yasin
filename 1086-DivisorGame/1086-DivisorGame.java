00a36d867c8cef2b060f8f18f821a97de3451a1a
YasinM007875 <muhammedyasin6369@gmail.com>
2026-08-11 14:19:57 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,10 @@
+// Last updated: 8/11/2026, 2:20:03 PM
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0)
+        return true;
+        else
+        return false;
+        
+    }
+}
