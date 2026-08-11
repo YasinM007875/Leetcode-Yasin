e0cedada7a200a410f50e899b7044285c3dd079c
YasinM007875 <muhammedyasin6369@gmail.com>
2026-08-11 14:20:51 +0530

    Create 874-BackspaceStringCompare

---
+++ 874-BackspaceStringCompare/874-BackspaceStringCompare.java
@@ -0,0 +1,18 @@
+// Last updated: 8/11/2026, 2:20:57 PM
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+       return compute(S).equals(compute(T));
+    }
+    
+    private Stack<Character> compute(String S) {
+        Stack<Character> stack = new Stack();
+        for (char c : S.toCharArray()) {
+            if (c != '#') {
+                stack.push(c);
+            } else if (!stack.isEmpty()) {
+                stack.pop();
+            }
+        }
+        return stack;
+    }
+}
