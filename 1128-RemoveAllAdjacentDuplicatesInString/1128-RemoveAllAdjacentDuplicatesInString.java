9f3b98e777a9f6febddde1df07a0b8d45dee1ddc
YasinM007875 <muhammedyasin6369@gmail.com>
2026-08-11 14:19:44 +0530

    Create 1128-RemoveAllAdjacentDuplicatesInString

---
+++ 1128-RemoveAllAdjacentDuplicatesInString/1128-RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,15 @@
+// Last updated: 8/11/2026, 2:19:50 PM
+class Solution {
+        public String removeDuplicates(String S) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : S.toCharArray()) {
+            int size = sb.length();
+            if (size > 0 && sb.charAt(size - 1) == c) {
+                sb.deleteCharAt(size - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
