8312ed2cf03a490ca0181c69262aefdaa2e6c888
YasinM007875 <muhammedyasin6369@gmail.com>
2026-08-11 14:16:57 +0530

    Create 1951-FindTheWinnerOfTheCircularGame

---
+++ 1951-FindTheWinnerOfTheCircularGame/1951-FindTheWinnerOfTheCircularGame.java
@@ -0,0 +1,16 @@
+// Last updated: 8/11/2026, 2:17:03 PM
+class Solution {
+    public int findTheWinner(int n, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1;i<=n;i++){
+            q.add(i);
+        }
+        while(q.size()!=1){
+            for(int i=0;i<k-1;i++)
+                q.add(q.poll());
+            q.poll();
+        }
+        return q.peek();
+        
+    }
+}
