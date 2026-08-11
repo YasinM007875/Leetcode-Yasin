7bbb5c7fb0a2871c9fe9a74d362fe159717e3a51
YasinM007875 <muhammedyasin6369@gmail.com>
2026-08-11 14:15:51 +0530

    Create 2748-CalculateDelayedArrivalTime

---
+++ 2748-CalculateDelayedArrivalTime/2748-CalculateDelayedArrivalTime.java
@@ -0,0 +1,9 @@
+// Last updated: 8/11/2026, 2:15:57 PM
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        
+        return (arrivalTime + delayedTime) % 24;
+    }
+}
+        
+    
