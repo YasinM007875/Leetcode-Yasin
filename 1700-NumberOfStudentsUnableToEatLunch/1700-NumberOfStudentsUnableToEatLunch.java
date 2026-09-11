1efea15d707e0878c4d30e3283b1ec28b0af6e36
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:29:55 +0530

    Create 1700-NumberOfStudentsUnableToEatLunch

---
+++ 1700-NumberOfStudentsUnableToEatLunch/1700-NumberOfStudentsUnableToEatLunch.java
@@ -0,0 +1,25 @@
+// Last updated: 9/11/2026, 9:29:55 AM
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Stack<Integer> st = new Stack<>();
+        Queue<Integer> q = new LinkedList();
+        for(int i=sandwiches.length-1;i>=0;i--)
+            st.push(sandwiches[i]);
+        for (int stud : students)
+            q.add(stud);
+        int t=0;    
+        while(!q.isEmpty() && !st.isEmpty() && t<st.size()){
+            if(q.peek()==st.peek()){
+                st.pop();
+                q.poll();
+                t=0;
+            }
+            else{
+                t++;
+                q.add(q.poll());
+            }
+        }  
+        return q.size();      
+        
+    }
+}
