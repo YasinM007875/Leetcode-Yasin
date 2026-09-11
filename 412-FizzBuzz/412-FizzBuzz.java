06d6b0e24d1a1a2713ecf1af8a322bfd5c3711d8
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:33:39 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -0,0 +1,28 @@
+// Last updated: 9/11/2026, 9:33:39 AM
+
+        import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            boolean divisibleBy3 = (i % 3 == 0);
+            boolean divisibleBy5 = (i % 5 == 0);
+
+            if (divisibleBy3 && divisibleBy5) {
+                answer.add("FizzBuzz");
+            } else if (divisibleBy3) {
+                answer.add("Fizz");
+            } else if (divisibleBy5) {
+                answer.add("Buzz");
+            } else {
+                answer.add(Integer.toString(i));
+            }
+        }
+
+        return answer;
+    }
+}
+    
