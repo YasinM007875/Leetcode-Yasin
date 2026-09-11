086e7172d24283c1ffdf150962f458167012a69e
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:34:30 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,11 @@
+// Last updated: 9/11/2026, 9:34:30 AM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0)
+        return false;
+        else 
+        return true;
+
+        
+    }
+}
