b4696cf6819571c8973c79736bcca88936997785
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:28:11 +0530

    Create 3014-MinimumNumberOfPushesToTypeWordI

---
+++ 3014-MinimumNumberOfPushesToTypeWordI/3014-MinimumNumberOfPushesToTypeWordI.java
@@ -0,0 +1,13 @@
+// Last updated: 9/11/2026, 9:28:11 AM
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        int pushes = 0;
+
+        for (int i = 0; i < n; i++) {
+            pushes += (i / 8) + 1;
+        }
+
+        return pushes;
+    }
+}
