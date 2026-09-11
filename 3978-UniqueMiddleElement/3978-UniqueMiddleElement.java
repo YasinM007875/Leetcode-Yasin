7133cc8826164418dfbe6d5cc21de439a040bc7b
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:27:18 +0530

    Create 3978-UniqueMiddleElement

---
+++ 3978-UniqueMiddleElement/3978-UniqueMiddleElement.java
@@ -0,0 +1,13 @@
+// Last updated: 9/11/2026, 9:27:18 AM
+class Solution {
+    public boolean isMiddleElementUnique(int[] nums) {
+        int mid = nums[nums.length / 2];
+        int count = 0;
+        for(int num : nums) {
+            if(num == mid) {
+                count++;
+            }
+        }
+        return count == 1;
+    }
+}
