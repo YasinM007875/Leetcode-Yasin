34c63a07701914d881cb6beedaf4ea71b5cb35b9
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:30:25 +0530

    Create 1346-CheckIfNAndItsDoubleExist

---
+++ 1346-CheckIfNAndItsDoubleExist/1346-CheckIfNAndItsDoubleExist.java
@@ -0,0 +1,16 @@
+// Last updated: 9/11/2026, 9:30:25 AM
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && arr[i] == 2 * arr[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
