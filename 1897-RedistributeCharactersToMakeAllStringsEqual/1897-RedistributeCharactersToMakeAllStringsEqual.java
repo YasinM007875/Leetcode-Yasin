b4a88398576756bea54e70fb310b71ac0a12076a
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:29:28 +0530

    Create 1897-RedistributeCharactersToMakeAllStringsEqual

---
+++ 1897-RedistributeCharactersToMakeAllStringsEqual/1897-RedistributeCharactersToMakeAllStringsEqual.java
@@ -0,0 +1,20 @@
+// Last updated: 9/11/2026, 9:29:28 AM
+class Solution {
+    public boolean makeEqual(String[] words) {
+        int[] count = new int[26];
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                count[words[i].charAt(j) - 'a']++;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] % words.length != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
