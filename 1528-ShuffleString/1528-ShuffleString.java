f5344540968a835cfc1190502b4ebb449b312eec
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:30:09 +0530

    Create 1528-ShuffleString

---
+++ 1528-ShuffleString/1528-ShuffleString.java
@@ -0,0 +1,10 @@
+// Last updated: 9/11/2026, 9:30:09 AM
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] ans = new char[s.length()];
+        for(int i=0;i<s.length();i++){
+            ans[indices[i]]=s.charAt(i);
+        }
+        return new String(ans);
+    }
+}
