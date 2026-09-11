0c99dc7387b0f7429c32c0991bfe95214d44b878
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:31:28 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 9/11/2026, 9:31:28 AM
+class Solution {
+    public int fib(int n) {
+        if (n <= 1) return n;
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b;
+    }
+}
