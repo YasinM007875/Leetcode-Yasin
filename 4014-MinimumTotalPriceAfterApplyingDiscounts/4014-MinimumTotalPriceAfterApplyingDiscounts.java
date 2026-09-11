5246b3cb742daa60e67fa1b73923a0f69f21daa5
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:27:06 +0530

    Create 4014-MinimumTotalPriceAfterApplyingDiscounts

---
+++ 4014-MinimumTotalPriceAfterApplyingDiscounts/4014-MinimumTotalPriceAfterApplyingDiscounts.java
@@ -0,0 +1,20 @@
+// Last updated: 9/11/2026, 9:27:06 AM
+class Solution {
+    public double minPrice(int[] prices, int[] discounts) {
+        Arrays.sort(prices);
+        Arrays.sort(discounts);
+        double total = 0.0;
+        int i = prices.length-1;
+        int j = discounts.length-1;
+        while(i >= 0 && j >= 0) {
+            total+=prices[i]*(100.0-discounts[j]) / 100.0;
+            i--;
+            j--;
+        }
+        while(i>=0){
+            total+=prices[i];
+            i--;
+        }
+        return total;
+    }
+}
