862fdb61fb2cc1ecf4b8ed29205e5131a5354def
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:34:33 +0530

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.java
@@ -0,0 +1,36 @@
+// Last updated: 9/11/2026, 9:34:33 AM
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        HashMap<Character, String> map1 = new HashMap<>();
+        HashMap<String, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            if (map1.containsKey(ch)) {
+                if (!map1.get(ch).equals(word)) {
+                    return false;
+                }
+            } else {
+                map1.put(ch, word);
+            }
+
+            if (map2.containsKey(word)) {
+                if (map2.get(word) != ch) {
+                    return false;
+                }
+            } else {
+                map2.put(word, ch);
+            }
+        }
+
+        return true;
+    }
+}
