5090db524d6eaf3929d973e174d08984a8f4689e
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:33:12 +0530

    Create 459-RepeatedSubstringPattern

---
+++ 459-RepeatedSubstringPattern/459-RepeatedSubstringPattern.java
@@ -0,0 +1,7 @@
+// Last updated: 9/11/2026, 9:33:12 AM
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s + s;
+        return str.substring(1, str.length() - 1).contains(s);
+    }
+}
