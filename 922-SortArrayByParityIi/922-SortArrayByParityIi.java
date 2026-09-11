18715e8b49b36c119cd691a8507250a96b13576a
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:31:36 +0530

    Create 922-SortArrayByParityIi

---
+++ 922-SortArrayByParityIi/922-SortArrayByParityIi.java
@@ -0,0 +1,21 @@
+// Last updated: 9/11/2026, 9:31:37 AM
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        int even = 0;
+        int odd = 1;
+
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                ans[even] = num;
+                even += 2;
+            } else {
+                ans[odd] = num;
+                odd += 2;
+            }
+        }
+
+        return ans;
+    }
+}
