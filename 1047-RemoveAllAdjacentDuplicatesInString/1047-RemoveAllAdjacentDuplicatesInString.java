50c8ad60ca66c68d3c502df12899952b27baa71f
YasinM007875 <muhammedyasin6369@gmail.com>
2026-09-11 09:31:04 +0530

    Create 1047-RemoveAllAdjacentDuplicatesInString

---
+++ 1047-RemoveAllAdjacentDuplicatesInString/1047-RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,15 @@
+// Last updated: 9/11/2026, 9:31:04 AM
+class Solution {
+        public String removeDuplicates(String S) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : S.toCharArray()) {
+            int size = sb.length();
+            if (size > 0 && sb.charAt(size - 1) == c) {
+                sb.deleteCharAt(size - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
